af5ab6f7206fa8d49cca56d09cd3470846280e4d
Rob <lahockeyfan2@yahoo.com>
2021-06-16 21:36:16 -0700

    fixed update note issue.

--- src/com/scaha/beans/loiBean.java
+++ src/com/scaha/beans/loiBean.java
@@ -1764,7 +1764,8 @@ public void getClubID(){
  				CallableStatement cs = db.prepareCall("CALL scaha.saveNoteSuspend(?,?,?)");
  				cs.setString("innote", this.notes);
  				cs.setInt("personid", this.selectedplayer);
- 				cs.setInt("suspendloi", this.suspendloi);
+ 				/*cs.setInt("suspendloi", this.suspendloi);*/
+				cs.setInt("suspendloi", 0);
  				
     		    cs.executeQuery();
     			db.commit();
